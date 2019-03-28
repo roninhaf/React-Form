54d5dd96a8b3e5458cb1435b6b3cacdbde102e0a
roninhaf <roninhaf@gmail.com>
2019-03-28 11:17:02 -0400

    fixed syntax

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
     this.state = {
       firstName: "",
       lastName: ""
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    const {name, value} = event.target
+    const {name, value} = event.target;
     this.setState({
         [name]: value
     })
